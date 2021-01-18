7909274b4348848d000fe62b025c980890d58d84
Shamima Ali <snali@mun.ca>
2021-01-18 16:30:53 -0330

    Complete but needs testing.

--- Board.java
+++ Board.java
@@ -7,6 +7,8 @@
 public class Board {
 
     private char[][] board = new char[3][3];
+    char iInput;
+    char jInput;
 
     /*
      * TBD: Create additional private members if useful.
@@ -16,9 +18,15 @@ public class Board {
      * Construct an empty board (contains all space char's).
      */
     public Board() {
-        /*
-         * TBD
-         */
+    	 board[0][0] = ' ';
+    	 board[0][1] = ' ';
+    	 board[0][2] = ' ';
+    	 board[1][0] = ' ';
+    	 board[1][1] = ' ';
+    	 board[1][2] = ' ';
+    	 board[2][0] = ' ';
+    	 board[2][1] = ' ';
+    	 board[2][2] = ' ';   	 
     }
 
     /**
@@ -26,18 +34,16 @@ public class Board {
      * 'move' to generate this board's state.
      */
     public Board(Board other, Move move) {
-        /*
-         * TBD
-         */
+        this.board = other.board;
     }
 
     /**
      * Convert to a string that shows the board's state.
      */
+    @Override
     public String toString() {
-        /*
-         * TBD
-         */
+        String boardToShow = String.valueOf(board);
+        return boardToShow;
     }
 
     /**
@@ -45,17 +51,23 @@ public class Board {
      * be in the range [0, 2].
      */
     public char get(int i, int j) {
-        /*
-         * TBD
-         */
+        if ((i >= 0 && i <= 2) && (i >= 0 && i <= 2)) {
+        	this.iInput = (char)i;
+        	this.jInput = (char)j;
+        }
     }
     
     /**
      * @return true if there remain no empty spots on the board.
      */
     public boolean isFull() {
-        /*
-         * TBD
-         */
+        for(int i = 0; i <3; i++) {
+        	for(int j = 0; i <3; j++) {
+        		if(this.board[i][j] != ' ') {
+        			return true;
+        		}
+        	}
+        }
+        return false;
     }
 }
